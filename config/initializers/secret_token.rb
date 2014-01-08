a896b7ef0395734ad5f632b1798519cf96f1956a
MikeSanti <mikesanti@outlook.com>
2014-01-07 23:11:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrexApp::Application.config.secret_token = 'c92acacf4e97d46d350271f08f48eac69070d8dfc6c891eb768365a317c04e62fd92cac3223dbd0c17d58dae18bdd810e053aa9c0d469865463ee45147f98d23'
